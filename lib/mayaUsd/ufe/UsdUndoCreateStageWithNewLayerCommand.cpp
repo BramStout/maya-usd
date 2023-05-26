8c3a19616a537596377ee4c7b4d4e0aff25108eb
Jannik Frohne <jannik.frohne@autodesk.com>
2023-05-26 13:57:33 +0200

    MAYA-129059 - Make use of try/catch for undo cleanup.

--- lib/mayaUsd/ufe/UsdUndoCreateStageWithNewLayerCommand.cpp
+++ lib/mayaUsd/ufe/UsdUndoCreateStageWithNewLayerCommand.cpp
@@ -62,129 +62,127 @@ void UsdUndoCreateStageWithNewLayerCommand::execute()
         return;
     }
 
-    // Get a MObject from the parent scene item.
-    // Note: If and only if the parent is the world node, MDagPath::transform() will set status to
-    // kInvalidParameter. In this case MObject::kNullObj is returned, which is a valid parent
-    // object. Thus, kInvalidParameter will not be treated as a failure.
-    MStatus  status;
-    MDagPath parentDagPath = MayaUsd::ufe::ufeToDagPath(_parentItem->path());
-    MObject  parentObject = parentDagPath.transform(&status);
-    if (status != MStatus::kInvalidParameter && MFAIL(status)) {
-        return;
-    }
+    bool createTransformSuccess = false;
+    try {
+        // Get a MObject from the parent scene item.
+        // Note: If and only if the parent is the world node, MDagPath::transform() will set status
+        // to kInvalidParameter. In this case MObject::kNullObj is returned, which is a valid parent
+        // object. Thus, kInvalidParameter will not be treated as a failure.
+        MStatus  status;
+        MDagPath parentDagPath = MayaUsd::ufe::ufeToDagPath(_parentItem->path());
+        MObject  parentObject = parentDagPath.transform(&status);
+        if (status != MStatus::kInvalidParameter && MFAIL(status)) {
+            throw std::runtime_error("");
+        }
 
-    // Create a transform node.
-    // Note: It would be possible to create the transform and the proxy shape in one doIt() call of
-    // a single MDagModifier. However, doing so causes notifications to be sent in a different
-    // order, which triggers a `TF_VERIFY(g_StageMap.isDirty())` in StagesSubject::onStageSet().
-    // Using a separate MDagModifier to create the transform seems more robust and avoids triggering
-    // the TF_VERIFY.
-    MObject transformObj;
-    transformObj = _createTransformDagMod.createNode("transform", parentObject, &status);
-    if (MFAIL(status)) {
-        return;
-    }
-    TF_VERIFY(!transformObj.isNull());
-    status = _createTransformDagMod.doIt();
-    if (MFAIL(status)) {
-        return;
-    }
+        // Create a transform node.
+        // Note: It would be possible to create the transform and the proxy shape in one doIt() call
+        // of a single MDagModifier. However, doing so causes notifications to be sent in a
+        // different order, which triggers a `TF_VERIFY(g_StageMap.isDirty())` in
+        // StagesSubject::onStageSet(). Using a separate MDagModifier to create the transform seems
+        // more robust and avoids triggering the TF_VERIFY.
+        MObject transformObj;
+        transformObj = _createTransformDagMod.createNode("transform", parentObject, &status);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        TF_VERIFY(!transformObj.isNull());
+        status = _createTransformDagMod.doIt();
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        createTransformSuccess = true;
 
-    // Create a proxy shape.
-    MObject proxyShape;
-    proxyShape = _createProxyShapeDagMod.createNode("mayaUsdProxyShape", transformObj, &status);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    TF_VERIFY(!proxyShape.isNull());
+        // Create a proxy shape.
+        MObject proxyShape;
+        proxyShape = _createProxyShapeDagMod.createNode("mayaUsdProxyShape", transformObj, &status);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        TF_VERIFY(!proxyShape.isNull());
 
-    // Rename the transform and the proxy shape.
-    // Note: The transform is renamed twice. The first rename operation renames it from its default
-    // name "transform1" to "stage1". The number-suffix will be automatically incremented if
-    // necessary. The second rename operation renames it from "stageX" to "stage1". This doesn't do
-    // anything for the transform itself but it will adjust the number-suffix of the proxy shape
-    // according to the suffix of the transform, because they now share the common prefix "stage".
-    status = _createProxyShapeDagMod.renameNode(proxyShape, "stageShape1");
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    status = _createProxyShapeDagMod.renameNode(transformObj, "stage1");
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    status = _createProxyShapeDagMod.renameNode(transformObj, "stage1");
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
+        // Rename the transform and the proxy shape.
+        // Note: The transform is renamed twice. The first rename operation renames it from its
+        // default name "transform1" to "stage1". The number-suffix will be automatically
+        // incremented if necessary. The second rename operation renames it from "stageX" to
+        // "stage1". This doesn't do anything for the transform itself but it will adjust the
+        // number-suffix of the proxy shape according to the suffix of the transform, because they
+        // now share the common prefix "stage".
+        status = _createProxyShapeDagMod.renameNode(proxyShape, "stageShape1");
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        status = _createProxyShapeDagMod.renameNode(transformObj, "stage1");
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        status = _createProxyShapeDagMod.renameNode(transformObj, "stage1");
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
 
-    // Get the global `time1` object and its `outTime` attribute.
-    MSelectionList selection;
-    selection.add("time1");
-    MObject time1;
-    status = selection.getDependNode(0, time1);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    MFnDependencyNode time1DepNodeFn(time1, &status);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    MObject time1OutTimeAttr = time1DepNodeFn.attribute("outTime", &status);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
+        // Get the global `time1` object and its `outTime` attribute.
+        MSelectionList selection;
+        selection.add("time1");
+        MObject time1;
+        status = selection.getDependNode(0, time1);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        MFnDependencyNode time1DepNodeFn(time1, &status);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        MObject time1OutTimeAttr = time1DepNodeFn.attribute("outTime", &status);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
 
-    // Get the `time` attribute of the newly created mayaUsdProxyShape.
-    MDagPath proxyShapeDagPath = MDagPath::getAPathTo(proxyShape, &status);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    MFnDependencyNode proxyShapeDepNodeFn(proxyShapeDagPath.node(), &status);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    MObject proxyShapeTimeAttr = proxyShapeDepNodeFn.attribute("time", &status);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
+        // Get the `time` attribute of the newly created mayaUsdProxyShape.
+        MDagPath proxyShapeDagPath;
+        status = MDagPath::getAPathTo(proxyShape, proxyShapeDagPath);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        MFnDependencyNode proxyShapeDepNodeFn(proxyShapeDagPath.node(), &status);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        MObject proxyShapeTimeAttr = proxyShapeDepNodeFn.attribute("time", &status);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
 
-    // Connect `time1.outTime` to `proxyShapde.time`.
-    status
-        = _createProxyShapeDagMod.connect(time1, time1OutTimeAttr, proxyShape, proxyShapeTimeAttr);
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
+        // Connect `time1.outTime` to `proxyShapde.time`.
+        status = _createProxyShapeDagMod.connect(
+            time1, time1OutTimeAttr, proxyShape, proxyShapeTimeAttr);
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+
+        // Execute the operations.
+        status = _createProxyShapeDagMod.doIt();
+        if (MFAIL(status)) {
+            throw std::runtime_error("");
+        }
+        _success = true;
+
+        // Create a UFE scene item for the newly created mayaUsdProxyShape.
+        Ufe::Path proxyShapeUfePath = MayaUsd::ufe::dagPathToUfe(proxyShapeDagPath);
+        _insertedChild = Ufe::Hierarchy::createItem(proxyShapeUfePath);
+
+        // Refresh the cache of the stage map.
+        // When creating the proxy shape, the stage map gets dirtied and cleaned. Afterwards, the
+        // proxy shape is renamed. The stage map does not observe the Maya data model, so renaming
+        // does not dirty the stage map again. Thus, the cache is in an invalid state, where it
+        // contains the path of the proxy shape before it was renamed. Calling getProxyShape()
+        // refreshes the cache. See comments within UsdStageMap::proxyShape() for more details.
+        getProxyShape(proxyShapeUfePath);
+    } catch (const std::exception&) {
+        if (createTransformSuccess) {
+            _createTransformDagMod.undoIt();
+        }
     }
-
-    // Execute the operations.
-    status = _createProxyShapeDagMod.doIt();
-    if (MFAIL(status)) {
-        _createTransformDagMod.undoIt();
-        return;
-    }
-    _success = true;
-
-    // Create a UFE scene item for the newly created mayaUsdProxyShape.
-    Ufe::Path proxyShapeUfePath = MayaUsd::ufe::dagPathToUfe(proxyShapeDagPath);
-    _insertedChild = Ufe::Hierarchy::createItem(proxyShapeUfePath);
-
-    // Refresh the cache of the stage map.
-    // When creating the proxy shape, the stage map gets dirtied and cleaned. Afterwards, the proxy
-    // shape is renamed. The stage map does not observe the Maya data model, so renaming does not
-    // dirty the stage map again. Thus, the cache is in an invalid state, where it contains the
-    // path of the proxy shape before it was renamed. Calling getProxyShape() refreshes the cache.
-    // See comments within UsdStageMap::proxyShape() for more details.
-    getProxyShape(proxyShapeUfePath);
 }
 
 void UsdUndoCreateStageWithNewLayerCommand::undo()
